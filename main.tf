37b2f429658e3e0c447b88a202c41554a896df64
qasim-ahmed <105868063+qasim-ahmed@users.noreply.github.com>
2022-05-24 10:04:02 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -38,16 +38,10 @@ variable "databasetype" {
 }
 
 locals {
-  location="North Europe"
+  location="West Europe"
   password="${random_string.random.result}"
 }
 
-data "azurerm_subnet" "Subnet1" {
-    name = "Subnet1"
-    virtual_network_name = var.virtualnetworkname
-    resource_group_name = var.resourcename
-  
-}
 
 
 resource "random_string" "random" {
@@ -79,76 +73,129 @@ resource "azurerm_resource_group" "app_resource" {
 #   container_access_type = "public"
 # }
 
-resource "azurerm_network_security_group" "network_security_group" {
-  name                = "network-security-group"
-  location            = local.location
-  resource_group_name = var.resourcename
-}
 
+
+# Create virtual network
 resource "azurerm_virtual_network" "virtual_network" {
   name                = var.virtualnetworkname
-  location            = local.location
-  resource_group_name = var.resourcename
   address_space       = ["10.0.0.0/16"]
-  dns_servers         = ["10.0.0.4", "10.0.0.5"]
+  location            = azurerm_resource_group.app_resource.location
+  resource_group_name = azurerm_resource_group.app_resource.name
 
-  subnet {
-    name           = "Subnet1"
-    address_prefix = "10.0.2.0/24"
-    security_group = azurerm_network_security_group.network_security_group.id
+  tags = {
+    environment = "production"
   }
-
 }
 
+# Create subnet
+resource "azurerm_subnet" "network_subnet" {
+  name                 = "Subnet1"
+  resource_group_name  = azurerm_resource_group.app_resource.name
+  virtual_network_name = azurerm_virtual_network.virtual_network.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
 
+# Create public IPs
+resource "azurerm_public_ip" "public_ip" {
+  name                = "public-ip"
+  location            = azurerm_resource_group.app_resource.location
+  resource_group_name = azurerm_resource_group.app_resource.name
+  allocation_method   = "Dynamic"
+
+  tags = {
+    environment = "production"
+  }
+}
+
+# Create Network Security Group and rule
+resource "azurerm_network_security_group" "network_sec_group" {
+  name                = "network-security-group"
+  location            = azurerm_resource_group.app_resource.location
+  resource_group_name = azurerm_resource_group.app_resource.name
+
+  security_rule {
+    name                       = "SSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  tags = {
+    environment = "production"
+  }
+}
+
+# Create network interface
 resource "azurerm_network_interface" "network_interface" {
   name                = "network-interface"
-  location            = local.location
-  resource_group_name = var.resourcename
+  location            = azurerm_resource_group.app_resource.location
+  resource_group_name = azurerm_resource_group.app_resource.name
 
   ip_configuration {
-    name                          = "internal"
-    subnet_id                     = data.azurerm_subnet.Subnet1.id
+    name                          = "myNicConfiguration"
+    subnet_id                     = azurerm_subnet.network_subnet.id
     private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.public_ip.id
+  }
+
+  tags = {
+    environment = "production"
   }
 }
 
-# Linux Virtual Machine
+# Connect the security group to the network interface
+resource "azurerm_network_interface_security_group_association" "association" {
+  network_interface_id      = azurerm_network_interface.network_interface.id
+  network_security_group_id = azurerm_network_security_group.network_sec_group.id
+}
 
-resource "azurerm_linux_virtual_machine" "linux_virtual_machine" {
-  name                = "example-machine"
-  resource_group_name = var.resourcename
-  location            = local.location
-  size                = var.virtualmachinesize
-  admin_username      = "myadmin"
-  admin_password = local.password
-  network_interface_ids = [
-    azurerm_network_interface.network_interface.id,
-  ]
 
-#   admin_ssh_key {
-#     username   = "adminuser"
-#     public_key = file("~/.ssh/id_rsa.pub")
-#   }
+# Create Linux Virtual Machine
+resource "azurerm_linux_virtual_machine" "linux_machine" {
+  name                  = "linux-machine"
+  location              = azurerm_resource_group.app_resource.location
+  resource_group_name   = azurerm_resource_group.app_resource.name
+  network_interface_ids = [azurerm_network_interface.network_interface.id]
+  size                  =  var.virtualmachinesize
 
   os_disk {
+    name                 = "myOsDisk"
     caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
+    storage_account_type = "Premium_LRS"
   }
 
   source_image_reference {
     publisher = "Canonical"
     offer     = "UbuntuServer"
-    sku       = "16.04-LTS"
+    sku       = "18.04-LTS"
     version   = "latest"
   }
+
+  computer_name                   = "myvm"
+  admin_username                  = "azureuser"
+  admin_password = local.password
+  disable_password_authentication = false
+
+
+  tags = {
+    environment = "production"
+  }
 }
+
+
+
+
 
 # Creating one sample user once VM is provisioned
 
 resource "azurerm_virtual_machine_extension" "virtual_machine_extensions" {
   name                 = "virtual-machine-extensions"
-  virtual_machine_id   = azurerm_linux_virtual_machine.linux_virtual_machine.id
+  virtual_machine_id   = azurerm_linux_virtual_machine.linux_machine.id
   publisher            = "Microsoft.Azure.Extensions"
   type                 = "CustomScript"
   type_handler_version = "2.0"
@@ -164,68 +211,80 @@ SETTINGS
   }
 }
 
+resource "random_integer" "ri" {
+  min = 10000
+  max = 99999
+}
+
 
 resource "azurerm_cosmosdb_account" "cosmo_db_account" {
-  name                = "cosmo-db-account"
-  location            = local.location
+  name = "tfex-cosmos-db-${random_integer.ri.result}"
+  location = local.location
   resource_group_name = var.resourcename
-  offer_type          = "Standard"
-
+  offer_type = "Standard"
+  kind = "GlobalDocumentDB"
   enable_automatic_failover = true
-
-  consistency_policy {
-    consistency_level       = "BoundedStaleness"
-    max_interval_in_seconds = 300
-    max_staleness_prefix    = 100000
+consistency_policy {
+    consistency_level = "Session"
   }
-
+  
   geo_location {
-    location          = local.location
+    location = "australiasoutheast"
+    failover_priority = 1
+  }
+geo_location {
+    location = local.location
     failover_priority = 0
   }
 }
 
 
+
+
+
 resource "azurerm_cosmosdb_sql_database" "sql_database" {
   count = var.databasetype == "sql" ? 1: 0
   name                = "cosmos-sql-db"
-  resource_group_name = var.resourcename
-  account_name        = data.azurerm_cosmosdb_account.cosmo_db_account.name
+  resource_group_name = "${azurerm_cosmosdb_account.cosmo_db_account.resource_group_name}"
+  account_name        = "${azurerm_cosmosdb_account.cosmo_db_account.name}"
   throughput          = 400
 }
 
 
-resource "azurerm_cosmosdb_mongo_database" "example" {
+resource "azurerm_cosmosdb_mongo_database" "mongo_database" {
   count = var.databasetype == "mongo" ? 1: 0
   name                = "cosmos-mongo-db"
-  resource_group_name = var.resourcename
-  account_name        = data.azurerm_cosmosdb_account.cosmo_db_account.name
+  resource_group_name = "${azurerm_cosmosdb_account.cosmo_db_account.resource_group_name}"
+  account_name        = "${azurerm_cosmosdb_account.cosmo_db_account.name}"
   throughput          = 400
 }
 
 
-resource "azurerm_cosmosdb_gremlin_database" "example" {
+
+resource "azurerm_cosmosdb_gremlin_database" "gremlin_database" {
   count = var.databasetype == "gremlin" ? 1: 0
   name                = "cosmos-gremlin-db"
   resource_group_name = var.resourcename
-  account_name        = data.azurerm_cosmosdb_account.cosmo_db_account.name
+  account_name        = "${azurerm_cosmosdb_account.cosmo_db_account.name}"
   throughput          = 400
 }
 
 
-output "databasepassword" {
+output "databasepass" {
   value = local.password
 }
 
-output "dbpassword" {
+output "dbpwd" {
     value = random_string.random
 }
 
 output "databasekey" {
-    value = azurerm_cosmosdb_account.cosmo_db_account.primary_key
+    value = nonsensitive(azurerm_cosmosdb_account.cosmo_db_account.primary_key)
 }
 
 output "databaseconnectionstrings" {
-    value = azurerm_cosmosdb_account.cosmo_db_account.connection_string
+    value = nonsensitive(azurerm_cosmosdb_account.cosmo_db_account.connection_strings)
   
 }
+
+
